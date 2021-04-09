435ef9f4c3c7feead48096eccd560c672b3b586b
tech-doctor <55724699+tech-doctor@users.noreply.github.com>
2021-04-09 22:44:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+const  App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Happy coding</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
