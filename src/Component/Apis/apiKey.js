3fbf10c2f37350a2530d2e1ec1cb90888e3f65d3
tech-doctor <hilghsilk2damson@gmail.com>
2022-06-29 23:36:17 +0100

    update

--- src/Component/Apis/apiKey.js
+++ src/Component/Apis/apiKey.js
@@ -1,3 +1,13 @@
-export const apiKey = process.env.REACT_YOUTUBE_API_KEY;
+// export const apiKey = "AIzaSyBBASn1mcnOfUhGoCFX0DopkZHDo4cPDQQ";
 
-export const public_key = process.env.REACT_PAYSTACK_PUBLIC_KEY;
+// export const public_key = 'pk_test_469aa0e95eef18322cd1be290b3d2aeed089d350';
+
+
+export const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+export const public_key = process.env.REACT_APP_PAYSTACK_PUBLIC_KEY;
+
+
+// SKIP_PREFLIGHT_CHECK=true
+// REACT_APP_YOUTUBE_API_KEY = AIzaSyBBASn1mcnOfUhGoCFX0DopkZHDo4cPDQQ
+// REACT_APP_PAYSTACK_PUBLIC_KEY = pk_test_469aa0e95eef18322cd1be290b3d2aeed089d350
